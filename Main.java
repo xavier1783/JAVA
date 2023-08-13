2eb8621c173fe56a4b9f5ded540ebbf841c193d3
xavier1783 <122426891+xavier1783@users.noreply.github.com>
2023-08-13 22:09:01 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.util.Scanner;
+/*public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number-");
+    short a =sc.nextShort();
+    System.out.println("your number is-"+a);
+
+        }
+    }
+
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number-");
+    long a =sc.nextLong();
+    System.out.println("your number is-"+a);
+
+        }
+    }
+
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number-");
+    double a =sc.nextDouble();
+    System.out.println("enter second number -");
+    double b =sc.nextDouble();
+        double c=a+b;
+        System.out.println(+c);
+        }
+    }
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("write-");
+        String a =sc.nextLine();
+        System.out.println(a);
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number-");
+        boolean a =sc.nextBoolean();
+        System.out.println(a);
+
+    }
+}
+
+class Test
+{
+    int a=10;
+    void display()
+    {
+        System.out.println(a);
+    }
+    public static void main(String arg[]){
+        Test t=new Test();
+        Test t1=new Test();
+        System.out.println("the value of object t is-");
+        System.out.println(t.a);
+        t.display();
+        System.out.println("the value of object t1 is-");
+        System.out.println(t1.a);
+        t1.display();
+
+    }
+}
+
+class Student
+{
+    Scanner sc =new Scanner(System.in);
+    int Rollno;
+    String name;
+    void get(){
+        Rollno = sc.nextInt();
+        name = sc.nextLine();
+
+    }
+        public static void main(String[]args){
+        Student s1 =new Student();
+        Student s2=new Student();
+        s1.get();
+        s2.get();
+
+
+        }
+
+
+}
+
+ */
